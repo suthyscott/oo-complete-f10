da78fe48a7e31f7acaedf722d0037179344477e1
suthyscott <sutherlandscott21@gmail.com>
2021-09-23 12:17:57 -0600

    updated with lecture code

--- object-orientation.js
+++ object-orientation.js
@@ -1,79 +1,97 @@
 let person = {
-    firstName: "Chandler",
-    lastName: "Bong",
-    age: 34
+    firstName: "Scott",
+    lastName: "Sutherland",
+    age: 23
 }
 
-// console.log(person.firstName) // much more common
+// console.log(person.firstName)
+// console.log(person['firstName'])
 
-// console.log(person['firstName']) // less common
+// Destructuring
+
+const {firstName} = person
+// console.log(firstName)
 
 let meal = {
-    appetizer: 'chips & salsa',
-    entree: 'al pastor burrito',
-    dessert: 'churros',
-    drink: 'dr. pepper'
+    appetizer: 'Calamari',
+    entree: 'Steak',
+    dessert: 'Chocolate cake',
+    drink: 'Merlot wine'
 }
+
+// const {dessert} = meal 
+
 // console.log(dessert)
-// const {dessert} = meal
-// const {appetizer, entree} = meal
-// const {drink: bestSodaEver} = meal
+// console.log(meal)
 
-const myDrink = meal.drink
-const myAppetizer = meal.appetizer
-const myEntree = meal.entree
-const myDessert = meal.dessert
+// const {appetizer, entree, dessert, drink} = meal
 
-// const {appetizer: myAppetizer, entree: myEntree, dessert: myDessert, drink: myDrink} = meal
+const {drink: bestDrinkEver, entree: mainCourse} = meal
+// const bestDrinkEver = meal.drink
+// console.log(drink, meal)
 
-// const {appetizer, entree, dessert, drink: myDrink} = meal
-// console.log(bestSodaEver)
 
-// let person = {
-//     firstName: "Chandler",
-//     lastName: "Bong",
-//     age: 34
-// }
+for(let key in person){
+    // console.log(typeof key)
+    // console.log('object', person)
+    // console.log('value', person[key])
+}
+// Property name is the key
+// The value is the object at the key (person[key])
 
-// for(key in person){
-//     console.log(person[key])
-// }
+person.job = "Instructor"
+person.job = 'chicken herder'
 
-person.job = "Statistical Analysis and Data Reconfiguration" 
+person['pets'] = ['chicken', 'duck']
 
-person['pets'] = ["Chicken", 'Duck']
+person.height = 183
 
-let teams = {
-    teamOne: ['ryan', 'alex', 'wyatt', 'tj'],
-    teamTwo: ['henry', 'cole', 'charlie', 'zeke'],
-    teamThree: ['porter', 'blake', 'june', 'owen'],
-    teamFour: ['brian', 'riley', 'ezra', 'jordan'],
-    teamFive: ['grey', 'milo', 'mckay', 'leo']
-  }
+delete person.height
 
-delete teams.teamFour
-
-// console.log(teams)
+// console.log(person)
 
 
 class Dog {
     constructor(dogName, dogBreed, dogAge){
-        this.name = dogName
-        this.breed = dogBreed
-        this.age = dogAge
+        this.name = dogName, 
+        this.breed = dogBreed,
+        this.age = dogAge,
         this.hasFleas = false
     }
 
-    greeting(){
+    gotFleas(){
+        this.hasFleas = !this.hasFleas
+    }
+    bark(){
         console.log(`Hi, my name is ${this.name} and I am an ${this.age} year old ${this.breed}`)
     }
 }
 
-let lassie = new Dog('Lassie', 'Collie', 18)
+let lassie = new Dog("Lassie", "Collie", 18)
 let beethoven = new Dog('Beethoven', 'St. Bernard', 12)
-// lassie.greeting()
 
-for(key in lassie){
-    // console.log(key)
-    console.log(lassie[key])
+// beethoven.bark()
+// lassie.bark()
+
+// console.log(beethoven)
+// lassie.gotFleas()
+
+// console.log(Object.getOwnPropertyNames(new Dog))
+
+
+class Product {
+    constructor(productName, productPrice){
+        this.name = productName,
+        this.price = productPrice,
+        this.departments = []
+    }
+
+    addToDepartment(depName){
+        this.departments.push(depName)
+    }
 }
+
+let batmanToy = new Product('Batman Action Figure', 4.99)
+batmanToy.addToDepartment('Toys')
+batmanToy.addToDepartment('Kiosk')
+console.log(batmanToy)
